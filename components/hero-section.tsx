69350a86ac57bc315eedfad16ba48b2be0fa78dc
Harsh <harsh.gupta2211@gmail.com>
2024-05-18 21:09:09 +0530

    refactor: improve Hero section layout and responsiveness

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -4,14 +4,14 @@ import JoinButton from "./ui/join-button";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen mx-auto ">
-      <div className="flex flex-col text-[44px] leading-[50.6px] text-center font-ppeditorialnew font-normal max-w-[590px]">
-        <span className="flex flex-row items-left">
+    <div className="flex flex-col items-center justify-center h-screen mx-6 md:mx-auto ">
+      <div className="flex flex-col items-center justify-center text-center font-ppeditorialnew font-normal text-[36px] leading-[41.4px] md:text-[44px] md:leading-[50.6px] md:max-w-[590px]">
+        <div className="flex flex-row">
           <span>Find your holy-grail&nbsp;</span>
           {/* <span className="italic block" id="cycle"></span> Use this only when using CSS Transitions */}
           {/* <span className="italic block" id="cycle"></span> Use this only when using CSS Transitions */}
           {/* <span className="w-[210px] italic"> */}
-          <span className="italic">
+          <span className="hidden italic md:block">
             <ReactRotatingText
               items={[
                 "sunscreen",
@@ -26,10 +26,32 @@ export default function Hero() {
               cursor={false}
             />
           </span>
-        </span>
-        <span className=" text-center">by asking real people</span>
+        </div>
+        <div className="italic flex flex-col md:block">
+          <span className="italic md:hidden">
+            <ReactRotatingText
+              items={[
+                "sunscreen",
+                "lipstick",
+                "lip balm",
+                "cleanser",
+                "concealer",
+                "foundation",
+                "retinol",
+                "moisturizer",
+              ]}
+              cursor={false}
+            />
+            &nbsp;
+            <span className="text-center">by asking</span>
+          </span>
+          <span className="text-center hidden md:block">
+            by asking real people
+          </span>
+          <span className="text-center md:hidden">real people</span>
+        </div>
       </div>
-      <div className="my-7 text-[16px] leading-[20.8px] text-center tracking-[4%] max-w-[520px]">
+      <div className="my-7 mx-6 text-center text-[13px] leading-[16.9px] tracking-[4%] md:text-[16px] md:leading-[20.8px]  md:max-w-[520px] md:mx-auto">
         We match people who have the same skin tone, skin type, age, gender,
         weather—you get the gist. A bit like Bumble, but for beauty
       </div>
