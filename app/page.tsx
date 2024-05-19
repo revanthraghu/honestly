087a4ab78920c93321f6e121b64661ceec00f5ce
Harsh <harsh.gupta2211@gmail.com>
2024-05-19 23:31:03 +0530

    refactor: add Section3 component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,25 @@ import iceberg1Mobile from "@/public/background/iceberg1-mobile.svg";
 import iceberg1Web from "@/public/background/iceberg1-web.svg";
 import Hero from "@/components/hero-section";
 import Section2 from "@/components/section2";
+import Section3 from "@/components/section3";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className=" flex flex-col bg-[#AEDCEE]">
-      <Navbar/>
+    <main className="flex flex-col bg-[#AEDCEE]">
+      <Navbar />
       <Hero />
       <Image
         src={iceberg1Mobile}
         alt="background-image-1"
-        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 md:hidden"
+        className="bottom-0 left-1/2 absolute md:hidden transform -translate-x-1/2 translate-y-1/2"
         aria-hidden
         priority
       />
       <Image
         src={iceberg1Web}
         alt="background-image-1"
-        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 hidden md:block"
+        className="md:block bottom-0 left-1/2 absolute hidden transform -translate-x-1/2 translate-y-1/2"
         aria-hidden
       />
       <Section2 />
