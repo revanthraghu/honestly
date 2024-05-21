45d672ba312a74e7d94863e9cbd50177c7ff20cb
Harsh <harsh.gupta2211@gmail.com>
2024-05-22 00:38:11 +0530

    chore: solve type error

--- app/message/page.tsx
+++ app/message/page.tsx
@@ -1,25 +1,25 @@
-
 import Info from "./info";
 import Modal from "./modal";
 
 interface Props {
-    isMobile: boolean;
-    isMessageBoxOpen: boolean;
-    setIsMessageBoxOpen: (arg0: boolean) => void;
+  isMobile: boolean;
+  isMessageBoxOpen: boolean;
+  setIsMessageBoxOpen: (arg0: boolean) => void;
 }
 
-const Message = ({
-    isMobile,
-    isMessageBoxOpen,
-    setIsMessageBoxOpen,
-}: Props) => {
-    return (
-        <main className="md:fixed flex flex-col h-[100%] z-50 md:bg-none bg-gradient-to-b from-topShade to-bottomShade ">
-            <Modal isMobile={isMobile} isMessageBoxOpen={isMessageBoxOpen}
-                setIsMessageBoxOpen={setIsMessageBoxOpen} />
-            <Info />
-        </main>
-    );
+export default function Message({
+  isMobile,
+  isMessageBoxOpen,
+  setIsMessageBoxOpen,
+}: Props) {
+  return (
+    <main className="md:fixed flex flex-col h-[100%] z-50 md:bg-none bg-gradient-to-b from-topShade to-bottomShade ">
+      <Modal
+        isMobile={isMobile}
+        isMessageBoxOpen={isMessageBoxOpen}
+        setIsMessageBoxOpen={setIsMessageBoxOpen}
+      />
+      <Info />
+    </main>
+  );
 }
-
-export default Message;
