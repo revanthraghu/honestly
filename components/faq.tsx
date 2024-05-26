f48bb1a4106073565334536950c58d4572ef2847
rk-rishikesh <rvk.rishikeshkale@gmail.com>
2024-05-26 20:55:05 +0530

    Fixed Faq Alignment

--- components/faq.tsx
+++ components/faq.tsx
@@ -60,12 +60,12 @@ export default function Faq() {
                     <div className="h-[302px] w-full border-black border-[0.5px]">
                         <div onClick={handleMobileFaqOne} className={`${clickedMobileOne ? "bg-white" : ""} flex flex-row w-full`}>
                             <div className="w-full h-[73px] border-black border-[0.5px]">
-                                <div className="font-ppmori font-semibold text-[13px] leading-[26px] px-8 py-[24px]">Tell me more, I like to read long form</div>
+                                <div className="font-ppmori font-semibold text-[13px] leading-[26px] px-6 py-[24px]">Tell me more, I like to read long form</div>
                             </div>
                         </div>
                         <div className="flex flex-row w-full">
-                            <div onClick={handleMobileFaqTwo} className={`${clickedMobileTwo ? "bg-white" : ""}  flex flex-col gap-2  w-[163px] h-[83px] border-black border-[0.5px]`}>
-                                <div className=" font-ppmori font-semibold text-[13px] leading-[26px] px-8 py-[14px]">
+                            <div onClick={handleMobileFaqTwo} className={`${clickedMobileTwo ? "bg-white" : ""}  flex flex-col gap-2  w-1/2 h-[83px] border-black border-[0.5px]`}>
+                                <div className=" font-ppmori font-semibold text-[13px] leading-[26px] px-6 py-[14px]">
                                     <div className="font-thin">
                                         Work with us
                                     </div>
@@ -74,8 +74,8 @@ export default function Faq() {
                                     </div>
                                 </div>
                             </div>
-                            <div className={`flex flex-col gap-2  w-1/2 h-[83px] border-black border-[0.5px] border-r-[1px] ${clickedMobileThree ? "bg-white" : ""}`}>
-                                <div onClick={handleMobileFaqThree} className={`font-ppmori font-semibold text-[13px] leading-[26px] px-8 py-[14px]`}>
+                            <div className={`flex flex-col gap-2  w-1/2 h-[83px] border-black border-[0.5px] ${clickedMobileThree ? "bg-white" : ""}`}>
+                                <div onClick={handleMobileFaqThree} className={`font-ppmori font-semibold text-[13px] leading-[26px] px-6 py-[14px]`}>
                                     <div className="font-thin">
                                         Questions?
                                     </div>
@@ -88,7 +88,7 @@ export default function Faq() {
                         <div className="flex flex-row">
                             <div onClick={handleMobileFaqFour} className={`${clickedMobileFour ? "bg-white" : ""}  w-full h-[73px] border-black border-[0.5px]`}>
                                 <div className="flex items-center gap-2">
-                                    <div className="font-ppmori font-semibold text-[13px] leading-[26px] px-8 py-[24px]">
+                                    <div className="font-ppmori font-semibold text-[13px] leading-[26px] px-6 py-[24px]">
                                         Instagram
                                     </div>
                                     <div className="relative cursor-pointer">
@@ -98,7 +98,7 @@ export default function Faq() {
                             </div>
                             <div className="w-full h-[73px]">
                                 <div onClick={handleMobileFaqFive} className={`${clickedMobileFive ? "bg-white" : ""} h-[73px] border-black border-[0.5px] flex items-center gap-2`}>
-                                    <div className="font-ppmori font-semibold text-[13px] leading-[26px] px-8 py-[24px]">
+                                    <div className="font-ppmori font-semibold text-[13px] leading-[26px] px-6 py-[24px]">
                                         LinkedIn
                                     </div>
                                     <div className="relative cursor-pointer">
@@ -109,7 +109,7 @@ export default function Faq() {
                         </div>
 
                         <div className="flex justify-center items-center w-full h-[73px] border-black border-[0.5px]">
-                            <div className="font-ppmori font-semibold text-[13px] leading-[10px] px-8 py-[5px]">HONESTLY, NO RIGHTS RESERVED YET</div>
+                            <div className="font-ppmori font-semibold text-[13px] leading-[10px] px-auto py-[5px]">HONESTLY, NO RIGHTS RESERVED YET</div>
                         </div>
                     </div>
                 </div>
