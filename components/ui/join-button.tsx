ccdf789aab1eeebe179e97a26f08e67ae4d35d92
Harsh <harsh.gupta2211@gmail.com>
2024-06-11 12:09:56 +0530

    update join button link

--- components/ui/join-button.tsx
+++ components/ui/join-button.tsx
@@ -6,9 +6,17 @@ export default function JoinButton({
   children: String;
   showSpots?: boolean;
 }) {
+  const onClick = () => {
+    const url =
+      "https://api.whatsapp.com/send?phone=917483487046&text=%F0%9F%91%8B%20Hey%20Karishma%2C%20would%20love%20to%20join%20Honestly%20community.%20My%20name%20is%20"; // Replace with your desired URL
+    window.open(url, "_blank");
+  };
   return (
     <div className="flex flex-col items-center justify-center">
-      <Button className="bg-[#F00832] flex items-center justify-center p-0 m-0 w-[237px] h-[49px] rounded-[28px] shadow-custom border-black border hover:italic hover:bg-[#F00832] hover:shadow-none">
+      <Button
+        onClick={onClick}
+        className="bg-[#F00832] flex items-center justify-center p-0 m-0 w-[237px] h-[49px] rounded-[28px] shadow-custom border-black border hover:italic hover:bg-[#F00832] hover:shadow-none"
+      >
         {/* <div className=""> */}
         <span className="font-semibold inset-0 text-white leading-[14.7px] tracking-[2.5%] text-[14px]">
           {children}
