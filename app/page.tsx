27447bfa7e008b9ac3680263ed264b29bf1c2d00
Revanth Raghu <revanth@janani.life>
2024-06-16 15:50:21 +0530

    changed footer to reduce DOM size

--- app/page.tsx
+++ app/page.tsx
@@ -7,37 +7,30 @@ import ReviewSection from "@/components/section4-reviews";
 import Section5 from "@/components/section5";
 import Section6 from "@/components/section6";
 import Ticker from "@/components/ticker";
-import bgImg from "@/public/background/bg.webp";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col">
-      <div className="relative flex flex-col">
-        <Image
-          src={bgImg}
-          alt="background-image"
-          className="absolute bg-cover bg-center w-full h-full -z-50"
-          priority
-        />
-        <Navbar />
-        <Hero />
-        <Section3 />
-      </div>
-      <Ticker />
-      <ReviewSection />
-      <Section5 />
-      <div
-        className=""
-        style={{
-          background:
-            "linear-gradient(rgba(174, 220, 238, 1), rgba(244, 245, 240, 1))",
-        }}
-      >
-        <Section6 />
-        <Faq />
-        <Footer />
-      </div>
-    </main>
-  );
+    return (
+        <main className='flex flex-col'>
+            <div className='relative flex flex-col'>
+                <Image src={"/background/bg.webp"} fill alt='background-image' className='absolute bg-cover bg-center w-full h-full -z-50' priority />
+                <Navbar />
+                <Hero />
+                <Section3 />
+            </div>
+            <Ticker />
+            <ReviewSection />
+            <Section5 />
+            <div
+                className=''
+                style={{
+                    background: "linear-gradient(rgba(174, 220, 238, 1), rgba(244, 245, 240, 1))"
+                }}
+            >
+                <Section6 />
+                <Faq />
+                <Footer />
+            </div>
+        </main>
+    );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,11 @@
-import logoWebImg from "@/public/footer/logo-web.svg";
-import logoMobileImg from "@/public/footer/logo-mobile.svg";
+// import logoWebImg from "@/public/footer/logo-web.svg";
+// import logoMobileImg from "@/public/footer/logo-mobile.svg";
 import Image from "next/image";
 
 export default function Footer() {
     return (
-        <div>
-            <div className='justify-center hidden md:flex  w-full h-full p-8 mb-8'>
+        <>
+            {/* <div className='justify-center hidden md:flex  w-full h-full p-8 mb-8'>
                 <Image
                     // width={270.92}
                     // height={54}
@@ -13,16 +13,20 @@ export default function Footer() {
                     alt='logo'
                     className=''
                 />
-            </div>
-            <div className='md:hidden flex justify-center w-full h-full p-8 mb-8'>
-                <Image
+            </div> */}
+            <div className='flex justify-center w-full h-full p-8 mb-8'>
+                {/* <Image
                     // width={151}
                     // height={30.1}
                     src={logoMobileImg}
                     alt='logo'
                     className=''
-                />
+                /> */}
+                <picture>
+                    <source srcSet='/footer/logo-web.svg' media='(min-width: 768px)' />
+                    <img src='/footer/logo-mobile.svg' alt='honestly' className='w-auto' width={210} height={42} loading='lazy' />
+                </picture>
             </div>
-        </div>
+        </>
     );
 }
